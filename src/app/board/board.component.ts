88703abdcd5b0102016b4a129d12da7971f8b119
Yoann Fleury <yoann.fleury@yahoo.com>
2016-12-04 16:29:40 +0100

    feat(board): add board parsing

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,72 +8,43 @@ import { Type } from '../board/type';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
+  private static SIZE: number = 10;
+
   private board: Array<Array<Square>>;
 
   constructor() {
-    this.board = [
-      [
-        new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN),
-        new Square(Type.EMPTY),
-        new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY),
-        new Square(Type.BLACK_PAWN)
-      ],
-      [
-        new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY),
-        new Square(Type.BLACK_PAWN),
-        new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN),
-        new Square(Type.EMPTY)
-      ],
-      [
-        new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN),
-        new Square(Type.EMPTY),
-        new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY),
-        new Square(Type.BLACK_PAWN)
-      ],
-      [
-        new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY),
-        new Square(Type.BLACK_PAWN),
-        new Square(Type.EMPTY), new Square(Type.BLACK_PAWN), new Square(Type.EMPTY), new Square(Type.BLACK_PAWN),
-        new Square(Type.EMPTY)
-      ],
-      [
-        new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY),
-        new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY)
-      ],
-      [
-        new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY),
-        new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY), new Square(Type.EMPTY)
-      ],
-      [
-        new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN),
-        new Square(Type.EMPTY),
-        new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY),
-        new Square(Type.WHITE_PAWN)
-      ],
-      [
-        new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY),
-        new Square(Type.WHITE_PAWN),
-        new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN),
-        new Square(Type.EMPTY)
-      ],
-      [
-        new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN),
-        new Square(Type.EMPTY),
-        new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY),
-        new Square(Type.WHITE_PAWN)
-      ],
-      [
-        new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY),
-        new Square(Type.WHITE_PAWN),
-        new Square(Type.EMPTY), new Square(Type.WHITE_PAWN), new Square(Type.EMPTY), new Square(Type.WHITE_PAWN),
-        new Square(Type.EMPTY)
-      ]
-    ];
-   }
+    this.parseBoard([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]);
+  }
 
   ngOnInit() {
 
   }
 
+  public update(): void {
+    this.parseBoard([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  }
 
+  private parseBoard(squares: Array<number>): void {
+    if (squares.length !== BoardComponent.SIZE * 10 / 2) {
+      console.error('EXCEPTION : The number of squares should be equal to ' + (BoardComponent.SIZE * 10 / 2));
+    }
+
+    this.board = new Array();
+
+    let s = 0;
+    for (let i = 0; i < BoardComponent.SIZE ; i++) {
+
+      this.board[i] = new Array();
+
+      for (let j = 0; j < BoardComponent.SIZE; j++) {
+        if ((i + j) % 2 !== 0) {
+          this.board[i][j] = new Square(squares[s++]);
+        } else {
+          this.board[i][j] = new Square(Type.EMPTY);
+        }
+      }
+    }
+  }
 }
