f82a05f4f57cc641708d278d3b2230e49778a771
Yoann Fleury <yoann.fleury@yahoo.com>
2016-12-19 14:01:44 +0100

    feat(sass): better style for tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BoardComponent } from './board/board.component';
 import { AsideComponent } from './aside/aside.component';
 import { BubbleComponent } from './bubble/bubble.component';
@@ -14,7 +13,6 @@ import { BubbleService } from './bubble.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     BoardComponent,
     AsideComponent,
     BubbleComponent
